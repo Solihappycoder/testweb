f1b4614881c2cde27c0e8cdeb75e6878c75bc0cd
soli <163376618+Solihappycoder@users.noreply.github.com>
2024-12-01 12:38:21 +1030

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('/'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname, '../index.html');
